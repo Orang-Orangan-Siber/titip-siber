229a92fe5ddb04d2951b7ec61163974e45e46b46
Muhammad Fathi Farhat <mfathifarhat72@gmail.com>
2024-12-01 18:30:47 +0700

    bikin login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class AuthController extends Controller
@@ -14,5 +15,20 @@ class AuthController extends Controller
 
     public function registerView() {
         return Inertia::render('Auth/Register');
+    }
+
+    public function login(Request $request) {
+        $credentials = $request->validate([
+            'nim' => 'required|max:255',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/');
+        } else {
+            return back()->withErrors(['LoginError' => "The provided credentials do not match our records."]);
+        }
     }
 }
